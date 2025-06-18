d07f14b60a8175d5d349c0cc61fd8059c1801ab4
Aniket Anand <aniketanand118@gmail.com>
2025-06-18 08:18:15 +0530

    Final updation

--- app.py
+++ app.py
@@ -25,16 +25,16 @@ with open('scaler.pkl','rb') as file:
 st.title('Customer Churn Prediction')
 
 # user i/p
-geography = st.selectbox('Geography',label_encoder_geo.categories[0])
+geography = st.selectbox('Geography',label_encoder_geo.categories_[0])
 gender = st.selectbox('Gender',label_encoder_gender.classes_)
 age = st.slider('Age',18,92)
 balance = st.number_input('Balance')
 credit_score = st.number_input('Credit Score')
 estimated_salary = st.slider('Estimated Salary')
 tenure = st.slider('Tenure',0,10)
-num_of_products = st.slider('Number of Products',1,4)
+num_of_products = st.slider('Number of Products',1,4) ## Chamging
 has_cr_card = st.selectbox('Has Credit Card',[0,1])
-is_active_number = st.selectbox('Is Active Member',[0,1])
+is_active_member = st.selectbox('Is Active Member',[0,1])
 
 
 # prepare  i/p data
@@ -46,7 +46,7 @@ input_data =pd.DataFrame({
     'Balance' : [balance],
     'NumOfProducts' :[num_of_products],
     'HasCrCard' : [has_cr_card],
-    'IsActiveMember':[is_active_number],
+    'IsActiveMember':[is_active_member],
     'EstimatedSalary':[estimated_salary]
 })
 
